3b82b97e3cf82033f02b571b6f79cde241958101
joseluetich <josefinaluetich@gmail.com>
2021-04-05 15:02:32 -0300

    CAD-13 - Creates order

--- src/main/java/jms/dan/pedidos/rest/RestOrder.java
+++ src/main/java/jms/dan/pedidos/rest/RestOrder.java
@@ -1,4 +1,22 @@
 package jms.dan.pedidos.rest;
 
+import jms.dan.pedidos.domain.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
 public class RestOrder {
+    private static final List<Order> ordersList = new ArrayList<>();
+    private static Integer ID_GEN = 1;
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@RequestBody Order newOrder) {
+        newOrder.setId(ID_GEN++);
+        ordersList.add(newOrder);
+        return ResponseEntity.ok(newOrder);
+    }
 }
